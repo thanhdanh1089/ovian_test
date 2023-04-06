cf2faf140fd2684b49963acd9d03424d6e3d2728
Danh <thanhdanh1089@gmail.com>
2023-04-07 01:53:43 +0700

    remove no using function

--- lib/utils/Utils.dart
+++ lib/utils/Utils.dart
@@ -1,14 +1,6 @@
 import 'package:intl/intl.dart';
 
 class Utils {
-  static double setAverageRating(List<int> ratings) {
-    var avgRating = 0;
-    for (int i = 0; i < ratings.length; i++) {
-      avgRating = avgRating + ratings[i];
-    }
-    return double.parse((avgRating / ratings.length).toStringAsFixed(1));
-  }
-
   static String formatDateTime(int timestamp) {
     var dateTime = DateTime.fromMicrosecondsSinceEpoch(timestamp);
     return DateFormat('dd-MMM-yyyy HH:ss').format(dateTime);
